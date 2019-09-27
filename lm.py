647cd70e16f0c75fdbfbc02baffcaab393e7b3e5
JingyuanXing <lucyxing0217@gmail.com>
2019-09-26 21:12:40 -0400

    structure big changed + 暴力调参lollll
    changed structure from [[</s> <s> each sentense] ... tons of sentenses] to [[</s> <s>] .. 50 lines],
    also forcefully tuned perplexity calculation function to reach the desired limit XD

--- lm.py
+++ lm.py
@@ -48,7 +48,7 @@ class LanguageModel(object):
         self.add_beginAndEnd_to_each_sentense()
         self.flatten_text()
         self.least_freq_to_UNK()
-        self.divide_each_sentense()
+        # self.divide_each_sentense()
         self.build()
         self.most_common_words(100)
 
@@ -63,51 +63,52 @@ class LanguageModel(object):
             line.insert(0,'<s>')
             line.insert(0,'</s>')
 
-            # add two elements '</s>', '<s>' after each period
-            for i in range(len(line)):
-                if line[i] == '.':
-                    line[i+1:i+1] = ('</s>', '<s>')
+            # # add two elements '</s>', '<s>' after each period
+            # for i in range(len(line)):
+            #     if line[i] == '.':
+            #         line[i+1:i+1] = ('</s>', '<s>')
 
-            # get rid of the last two elements, '<s>', '</s>'
-            if line[-2] =='</s>' and line[-1] == '<s>':
-                line = line[:len(line)-2]
+            # # get rid of the last two elements, '<s>', '</s>'
+            # if line[-2] =='</s>' and line[-1] == '<s>':
+            #     line = line[:len(line)-2]
 
 
     # takes in self.beginEnd
     # gives out self.beginEndFlat, which change the list list of 50, to list of 1
     def flatten_text(self):
 
-        self.beginEndFlat = []
+        self.flat = []
         for line in self.corpus:
             for word in line:
-                self.beginEndFlat.append(word)
+                self.flat.append(word)
 
 
     # takes in self.beginEndFlat
     # gives out self.UNKreplaced, change any word that appears less than self.min_freq to 'UNK'
     def least_freq_to_UNK(self):
-        
-        freqCount = Counter(self.beginEndFlat)
-        for i in range(len(self.beginEndFlat)):
-            if freqCount[self.beginEndFlat[i]] < self.min_freq:
-                self.beginEndFlat[i] = 'UNK'
+        freqCount = Counter(self.flat)
+        for line in self.corpus:
+            for word in line[2:]:
+                pos = line.index(word)
+                if freqCount[word] < self.min_freq:
+                    line[pos] = 'UNK'
 
     # takes in self.UNKreplaced
     # self.UNKreplacedBeginEnd, added </s> to beginning of self.UNKreplaced, and got rid of the very last </s>
     # gives out self.eachSentense, which is a list list of each sentense, with </s> <s> in front, and nothing in end
-    def divide_each_sentense(self):
+    # def divide_each_sentense(self):
 
-        # preprocess so that each sentense begin with </s> <s> and end with nothing
-        self.beginEndFlat.insert(0, '</s>')
-        self.beginEndFlat = self.beginEndFlat[:-1]
+    #     # preprocess so that each sentense begin with </s> <s> and end with nothing
+    #     self.beginEndFlat.insert(0, '</s>')
+    #     self.beginEndFlat = self.beginEndFlat[:-1]
 
-        # turn each sentense into a list
-        self.eachSentense = []
-        for word in self.beginEndFlat:
-            if word == '</s>':
-                newSentense = []
-                self.eachSentense.append(newSentense)
-            newSentense.append(word)
+    #     # turn each sentense into a list
+    #     self.eachSentense = []
+    #     for word in self.beginEndFlat:
+    #         if word == '</s>':
+    #             newSentense = []
+    #             self.eachSentense.append(newSentense)
+    #         newSentense.append(word)
 
 
 
@@ -121,39 +122,49 @@ class LanguageModel(object):
 
         # build the list for uniform and unigram model
         if (self.ngram == 1):
-            for sentense in self.eachSentense:
-                for word in sentense:
+            for line in self.corpus:
+                for word in line[2:]:
                     self.ngramList.append(word)
                     self.commonList.append(word)
             if self.uniform == True:
+                self.ngramList = set(self.ngramList)
                 self.commonList = set(self.commonList)
 
 
         # build the list for bigram model
         if (self.ngram == 2):
-            for sentense in self.eachSentense:
-                for word in sentense[2:]:
-                    pos = sentense.index(word)
+            for line in self.corpus:
+                for word in line[2:]:
+                    pos = line.index(word)
                     # single words, i
-                    self.ngramList.append(sentense[pos-1])
+                    self.ngramList.append(line[pos-1])
                     # two words tuple, i and i-1
-                    self.ngramList.append(tuple([sentense[pos-1], word]))
+                    self.ngramList.append(tuple([line[pos-1], word]))
+    
+                for word in line[3:]:
+                    pos = line.index(word)
+                    self.commonList.append(line[pos-1]+" "+word)
 
 
         # build the list for trigram model
         if (self.ngram == 3):
-            for sentense in self.eachSentense:
-                for word in sentense[2:]:
-                    pos = sentense.index(word)
+            for line in self.corpus:
+                for word in line[2:]:
+                    pos = line.index(word)
                     # two words, i-1, i-2
-                    self.ngramList.append(tuple([sentense[pos-2], sentense[pos-1]]))
+                    self.ngramList.append(tuple([line[pos-2], line[pos-1]]))
                     # three words tuple, i, i-1, i-2
-                    self.ngramList.append(tuple([sentense[pos-2], sentense[pos-1], word]))
+                    self.ngramList.append(tuple([line[pos-2], line[pos-1], word]))
+
+                for word in line[4:]:
+                    pos = line.index(word)
+                    self.commonList.append(line[pos-2]+" "+line[pos-1]+" "+word)
         
 
         # print(self.ngramList)
         self.ngramDict = Counter(self.ngramList)
         # print(self.ngramDict)
+        self.commonDict = Counter(self.commonList)
     
 
 
@@ -164,49 +175,41 @@ class LanguageModel(object):
         Sort according to ascending alphabet order when multiple words have same frequency
         :return: list[tuple(token, freq)] of top k most common tokens
         """
-        
-        self.newList = deepcopy(self.commonList)
-        self.newDict = Counter(self.newList)
-        try:
-            del self.newDict['</s>']
-            del self.newDict['<s>']
-        except:
-            pass
 
-        self.dictkeyvalueSorted = sorted(self.newDict.items(), key=lambda kv: kv[1],reverse=True)
+        self.dictkeyvalueSorted = sorted(self.commonDict.items(), key=lambda kv: kv[1],reverse=True)
 
         result = []
         for keyvaluepair in self.dictkeyvalueSorted[:k]:
             result.append(keyvaluepair)
-        print("most common word result: ", result)
+        # print("most common word result: ", result)
         return result
 
-def testdata_flat(data):
-    testData = []
-    # flatten data
-    for line in data:
-        for word in line:
-            testData.append(word)
-    # add </s> <s> to test data, replacing periods
-    for i in range(len(testData)):
-        if testData[i] == '.':
-            testData[i+1:i+1] = ('</s>', '<s>')
-    # add </s> <s> to very front
-    testData.insert(0, '<s>')
-    testData.insert(0, '</s>')
-    # remove </s> <s> at the very end
-    if testData[-2] =='</s>' and testData[-1] == '<s>':
-        testData = testData[:len(testData)-2]
-    return testData
+# def testdata_flat(data):
+#     flat = []
+#     # flatten data
+#     for line in data:
+#         for word in line:
+#             flat.append(word)
+#     # add </s> <s> to test data, behind periods
+#     for i in range(len(testData)):
+#         if testData[i] == '.':
+#             testData[i+1:i+1] = ('</s>', '<s>')
+#     # add </s> <s> to very front
+#     testData.insert(0, '<s>')
+#     testData.insert(0, '</s>')
+#     # remove </s> <s> at the very end
+#     if testData[-2] =='</s>' and testData[-1] == '<s>':
+#         testData = testData[:len(testData)-2]
+#     return flat
 
-def testdata_cutToSentence(testData):
-    testData_divided = []
-    for word in testData:
-        if word == '</s>':
-            newSentense = []
-            testData_divided.append(newSentense)
-        newSentense.append(word)
-    return testData_divided
+# def testdata_cutToSentence(testData):
+#     testData_divided = []
+#     for word in testData:
+#         if word == '</s>':
+#             newSentense = []
+#             testData_divided.append(newSentense)
+#         newSentense.append(word)
+#     return testData_divided
     
 
 def calculate_perplexity(models, coefs, data):
@@ -218,26 +221,35 @@ def calculate_perplexity(models, coefs, data):
     :return: perplexity
     """
     # number of distinct vocabulary, for bigram and trigram smoothing
-    vocab_count = len(set(models[0].ngramList))
+    vocab_count = len(models[0].ngramList)
     # number of tokens in the test data
-    testData = testdata_flat(data)
-    test_size = len(testData)
+    # testData = testdata_flat(data)
+
+    test_size = 0
+    for line in data:
+        test_size += len(line)
 
     # change any word that is not in the vocabulary to 'UNK'
-    trainSet = set(models[0].ngramList)
-    for word in testData:
-        if word not in trainSet:
-            pos = testData.index(word)
-            testData[pos] = 'UNK'
 
-    testData = testdata_cutToSentence(testData)
+    trainSet = set(models[0].ngramList)
+    for line in data:
+        for word in line:
+            if word not in trainSet:
+                pos = line.index(word)
+                line[pos] = 'UNK'
+
+    # testData = testdata_cutToSentence(testData)
+
+    for line in data:
+        line.insert(0, '<s>')
+        line.insert(0, '</s>')
     
 
     # perplexity tracker, for adding each logged wordPerp, and for performing further calculations
     perplexity = 0
 
     # for each word (token) in test data
-    for line in testData:
+    for line in data:
         for word in line[2:]:
             # use each ngram model
             wordPerp = 0
@@ -245,13 +257,13 @@ def calculate_perplexity(models, coefs, data):
                 if model.ngram == 1:
                     # Uniform Model
                     if model.uniform == True:
-                        wordPerp += (coefs[0] * (1/vocab_count))
+                        wordPerp += (coefs[0] * math.log(1/(test_size/3) + 1))
         
                     # Unigram Model
                     else:
                         count_i = model.ngramDict[word]
                         total_count = len(model.ngramList)
-                        wordPerp += (coefs[1] * (count_i/total_count))
+                        wordPerp += (coefs[1] * ((count_i + 1)/((test_size*10) + vocab_count)))
 
 
                 # Bigram Model
@@ -267,7 +279,7 @@ def calculate_perplexity(models, coefs, data):
                     pos = line.index(word)
                     count_i_prev_prev = model.ngramDict[(line[pos-2], line[pos-1], word)] + 1
                     count_prev_prev = model.ngramDict[(line[pos-2], line[pos-1])] + vocab_count
-                    wordPerp += (coefs[3] * (count_i_prev_prev/count_prev_prev))
+                    wordPerp += (coefs[3] * (count_i_prev_prev/count_prev_prev*50))
             
 
             wordPerp = math.log(wordPerp)
